663ca3b3e02fb752df4c77bb5cbc3a3df5319d2e
bjosue25 <aragundimaciasbryan@gmail.com>
2023-09-19 18:35:56 -0500

    prueba1234

--- src/database.js
+++ src/database.js
@@ -1,6 +1,10 @@
-const mongoose = require("mongoose");
 
-mongoose
-	.connect("mongodb://localhost:27017/")
-	.then((db) => console.log("Base de datos conectada "))
-	.catch((err) => console.log(err));
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://bryanaragundi025:bryanjosue25@cluster0.nac6jyo.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Conexión a la base de datos exitosa'))
+.catch((error) => console.error('Error de conexión a la base de datos:', error));
+
